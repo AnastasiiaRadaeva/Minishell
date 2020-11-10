c1a02f44e866d4529b892d329d5dc1028096dc6f
Anastasiia Radaeva <kbatwoma@student.21-school.ru>
2020-11-10 12:09:53 +0300

    Update program_exit.h

--- header/program_exit.h
+++ header/program_exit.h
@@ -17,8 +17,19 @@
 
 # define MALLOC_1 "Error : Malloc fail (init_struct_commands)"
 # define MALLOC_2 "Error : Malloc fail (ft_command_line)"
+# define MALLOC_3 "Error : Malloc fail (ft_devider)"
+# define MALLOC_4 "Error : Malloc fail (ft_parse_mini_line)"
+# define MALLOC_5 "Error : Malloc fail (ft_cd)"
+# define MALLOC_6 "Error : Malloc fail (ft_join_all_args)"
+# define MALLOC_7 
+# define MALLOC_8 
 # define GNL_1 "Error : GNL fail (read_cmd)"
 # define CMD_NO_FOUND "minishell: command not found: "
+# define PWD_GETCWD "Error : too many characters in the path (getcwd)"
+# define PWD_MANY_ARGS "pwd: too many arguments"
+# define CD_STR_NOT "cd: string not in pwd: "
+# define CD_MANY_ARGS "cd: too many arguments"
+# define CD_NO_SUCH_F_D "cd: no such file or directory: "
 
 void	error_output(t_commands *cmd, char *str);
 int		program_exit(t_commands *cmd, int num);
