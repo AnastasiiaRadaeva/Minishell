6eb5a0a0e7c9ef1a29359256f3e805583f51ac8a
CiberAdventureCat <kbatwoma@student.21-school.ru>
2020-11-19 17:31:51 +0300

    cd_needed_check

--- srcs/builtin_commands/cd.c
+++ srcs/builtin_commands/cd.c
@@ -6,50 +6,68 @@
 /*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 11:22:15 by kbatwoma          #+#    #+#             */
-/*   Updated: 2020/11/17 16:51:49 by kbatwoma         ###   ########.fr       */
+/*   Updated: 2020/11/19 17:27:56 by kbatwoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../../header/minishell.h"
+#include <stdio.h> //
 
-static void	change_var(t_commands *cmd, t_data *all)
+void	error_output(t_commands *cmd, char *str); //
+void	ft_putstr(char *s); //
+char	*ft_strdup(const char *src); //
+char	*ft_strjoin(char const *s1, char const *s2); //
+size_t		ft_strlen(const char *s); //
+char		*ft_strndup(const char *src, size_t n); //
+
+static void	change_var(t_commands *cmd, t_data **all)
 {
 	char	*tmp;
+	char	*tmp_2;
 
-	tmp = all->envp[all->old_pwd];
-	if (!(all->envp[all->old_pwd] = ft_strdup(all->envp[all->current_pwd])))
+	tmp = (*all)->envp[(*all)->old_pwd];
+	if (!((*all)->envp[(*all)->old_pwd] = ft_strjoin("OLD", (*all)->envp[(*all)->current_pwd])))
 		error_output(cmd, MALLOC_11);
 	free(tmp);
-	tmp = all->envp[all->current_pwd];
-	if (!(all->envp[all->current_pwd] = ft_strdup(cmd->lst->content)))
+	if (!(tmp_2 = (char *)malloc(sizeof(char) * 100)))
+		error_output(cmd, MALLOC_11);
+	getcwd(tmp_2, 100);
+	tmp = (*all)->envp[(*all)->current_pwd];
+	if (!((*all)->envp[(*all)->current_pwd] = ft_strjoin("PWD=", ft_strndup(tmp_2, ft_strlen(tmp_2)))))
 		error_output(cmd, MALLOC_11);
 	free(tmp);
+	free(tmp_2);
 }
 
-void	ft_cd(t_commands *cmd, t_data *all)
+void	ft_cd(t_commands **cmd, t_data *all)
 {
 	char	*tmp_path;
 
-	if (cmd->count_args == 0)
-		cmd->lst->content = all->envp[all->home_dir] + 5;
-	if (cmd->count_args == 2)
+	if ((*cmd)->count_args == 0)
+	{
+		if (!((*cmd)->lst = (t_list *)malloc(sizeof(t_list))))
+				error_output(*cmd, MALLOC_11); //нужен маллок 14
+		((*cmd)->lst->content) = all->envp[all->home_dir] + 5;
+		(*cmd)->lst->next = NULL;
+	}
+	if ((*cmd)->count_args == 2)
 	{
 		ft_putstr(CD_STR_NOT);
-		error_output(cmd, cmd->lst->content);
+		error_output(*cmd, ((*cmd)->lst->content));
 	}
-	if (cmd->count_args > 2)
-		error_output(cmd, CD_MANY_ARGS);
-	if (cmd->lst->content[0] == '~')
+	if ((*cmd)->count_args > 2)
+		error_output(*cmd, CD_MANY_ARGS);
+	if (((char*)(*cmd)->lst->content)[0] == '~')
 	{
-		if (!(tmp_path = ft_strjoin(all->envp[all->home_dir] + 5, cmd->lst->content + 1)))
-			error_output(cmd, MALLOC_5);
-		free(cmd->lst->content);
-		cmd->lst->content = tmp_path;
+		if (!(tmp_path = ft_strjoin(all->envp[all->home_dir] + 5, (char *)(*cmd)->lst->content + 1)))
+			error_output(*cmd, MALLOC_5);
+		free((char *)((*cmd)->lst->content));
+		((*cmd)->lst->content) = tmp_path;
 	}
-	if (chdir(cmd->lst->content) != 0)
+	if (chdir((char *)(*cmd)->lst->content) != 0)
 	{
 		ft_putstr(CD_NO_SUCH_F_D);
-		error_output(cmd, MALLOC_5);
+		error_output(*cmd, (*cmd)->lst->content);
 	}
-	change_var(cmd, all);
+	change_var(*cmd, &all);
 }
---
+++ srcs/builtin_commands/main_check.c
@@ -0,0 +1,292 @@
+# include "../../header/minishell.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int					ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*ps1;
+	unsigned char	*ps2;
+
+	i = 0;
+	ps1 = (unsigned char *)s1;
+	ps2 = (unsigned char *)s2;
+	while ((ps1[i] != '\0' || ps2[i] != '\0') && i < n)
+	{
+		if (ps1[i] != ps2[i])
+			return (ps1[i] - ps2[i]);
+		i++;
+	}
+	return (0);
+}
+
+size_t		ft_strlen(const char *s)
+{
+	size_t i;
+
+	i = -1;
+	while (s[++i])
+		;
+	return (i);
+}
+
+char		*ft_strndup(const char *src, size_t n)
+{
+	char	*new_array;
+	size_t	i;
+	size_t	size;
+
+	i = 0;
+	size = 0;
+	if (!(new_array = (char *)malloc(sizeof(char) * (ft_strlen(src) + 1))))
+		return (NULL);
+	while (src[i] && size != n)
+	{
+		new_array[i] = src[i];
+		i++;
+		size++;
+	}
+	new_array[i] = '\0';
+	return (new_array);
+}
+
+size_t		ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	n;
+	size_t	i;
+	size_t	size_d;
+
+	n = size;
+	i = 0;
+	if (n != 0)
+	{
+		while (src[i] != '\0' && --n != 0)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		if (src[i] == '\0')
+			dest[i] = '\0';
+	}
+	if (n == 0 && size != 0)
+		dest[i] = '\0';
+	size_d = ft_strlen(src);
+	return (size_d);
+}
+
+size_t			ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	res;
+
+	i = 0;
+	while (dest[i] != '\0')
+		++i;
+	res = 0;
+	while (src[res] != '\0')
+		++res;
+	if (size <= i)
+		res += size;
+	else
+		res += i;
+	j = 0;
+	while (src[j] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (res);
+}
+
+char				*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	lens1;
+	size_t	lens2;
+	char	*nstr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	lens1 = ft_strlen(s1);
+	lens2 = ft_strlen(s2);
+	if (!(nstr = (char *)malloc(sizeof(char) * (lens1 + lens2 + 1))))
+		return (NULL);
+	ft_strlcpy(nstr, s1, lens1 + 1);
+	ft_strlcat(nstr, s2, lens1 + lens2 + 1);
+	return (nstr);
+}
+
+char		*ft_strdup(const char *src)
+{
+	char	*p;
+	size_t	len;
+
+	len = ft_strlen(src);
+	if (!(p = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	ft_strlcpy(p, src, len + 1);
+	return (p);
+}
+
+void		ft_putstr_fd(char *s, int fd)
+{
+	if (s)
+	{
+		write(fd, s, ft_strlen(s));
+	}
+}
+
+void		ft_putstr(char *s)
+{
+	if (s)
+	{
+		write(1, s, ft_strlen(s));
+	}
+}
+
+void	error_output(t_commands *cmd, char *str)
+{
+	free(cmd);
+	ft_putstr_fd(str, 2);
+	write(2, "\n", 1);
+	exit(0);
+}
+
+static char	**copy_double_array(char **origin, t_commands *cmd)
+{
+	char	**duplicate;
+	int		num_of_str;
+	int		index;
+
+	num_of_str = 0;
+	index = 0;
+	while (origin[num_of_str])
+		num_of_str++;
+	if (!(duplicate = (char **)malloc(sizeof(char *) * (num_of_str + 1))))
+		error_output(cmd, MALLOC_10);
+	duplicate[num_of_str] = NULL;
+	while (index < num_of_str)
+	{
+		if (!(duplicate[index] = ft_strdup(origin[index])))
+			error_output(cmd, MALLOC_10);
+		index++;
+	}
+	return (duplicate);
+}
+
+static void	data_filling(t_data **all)
+{
+	int		index;
+
+	index = 0;
+	while ((*all)->envp[index])
+	{
+		if (ft_strncmp((*all)->envp[index], "HOME=", 5) == 0)
+			(*all)->home_dir = index;
+		if (ft_strncmp((*all)->envp[index], "PWD=", 4) == 0)
+			(*all)->current_pwd = index;
+		if (ft_strncmp((*all)->envp[index], "OLDPWD=", 7) == 0)
+			(*all)->old_pwd = index;
+		if (ft_strncmp((*all)->envp[index], "_=", 2) == 0)
+			(*all)->env_var = index;
+		index++;
+	}
+	(*all)->count_str = index;
+}
+
+void	create_env_v(t_data **all, char **envp_baze, t_commands *cmd)
+{
+	if (!((*all) = (t_data *)malloc(sizeof(t_data))))
+		error_output(cmd, MALLOC_9);
+	(*all)->envp = NULL;
+	(*all)->home_dir = -1;
+	(*all)->current_pwd = -1;
+	(*all)->old_pwd = -1;
+	(*all)->env_var = -1;
+	(*all)->envp = copy_double_array(envp_baze, cmd);
+	(*all)->count_str = -1;
+	(*all)->error_string = NULL;
+	data_filling(all);
+}
+
+void	create_cmd(t_commands **cmd, char **argv, int argc)
+{
+	t_list	*lst;
+	t_list	*temp_lst;
+	int		index;
+
+	index = 0;
+	if (!((*cmd) = (t_commands *)malloc(sizeof(t_commands))))
+		error_output(*cmd, MALLOC_11); //нужен маллок 13
+	(*cmd)->cmd = NULL;
+	(*cmd)->num_cmd = 0;
+	(*cmd)->cmd_dir = NULL;
+	(*cmd)->count_args = argc;
+	(*cmd)->type_redir = 0;
+	(*cmd)->invalid = 0;
+	if (argc > 0)
+	{
+		if (!((*cmd)->lst = (t_list *)malloc(sizeof(t_list))))
+				error_output(*cmd, MALLOC_11); //нужен маллок 13
+		(*cmd)->lst->next = NULL;
+		(*cmd)->lst->content = argv[index];
+		lst = (*cmd)->lst;
+		index++;	
+		while (index < argc)
+		{
+			if (!(temp_lst = (t_list *)malloc(sizeof(t_list))))
+				error_output(*cmd, MALLOC_11); //нужен маллок 13
+			temp_lst->next = NULL;
+			temp_lst->content = argv[index];
+			lst->next = temp_lst;
+			lst = lst->next;
+			index++;
+		}
+		lst->next = NULL;
+	}
+	(*cmd)->redir = NULL;
+	(*cmd)->pipe = NULL;
+	(*cmd)->next = NULL;
+}
+
+char	**ft_env(t_commands *cmd, t_data *all);
+void	ft_cd(t_commands **cmd, t_data *all);
+char	*ft_echo(t_commands *cmd);
+void	ft_export(t_commands *cmd, t_data *all);
+char	*ft_pwd(t_commands *cmd);
+
+#include <stdio.h>
+
+int    			main(int argc, char **argv, char **envp)
+{
+    t_commands	*cmd;
+	t_data		*all;
+	char		**copy_args;
+	
+	argc = 1;
+	(void)argv;
+	char s[100];
+	if (!(copy_args = (char **)malloc(sizeof(char *) * argc)))
+		return (0);
+	int index = 0;
+	while (index < argc)
+	{
+		if (!(copy_args[index] = ft_strdup("..")))
+			return (0);
+		index++;
+	}
+	all = NULL;
+	cmd = NULL;
+	create_env_v(&all, envp, cmd);
+	create_cmd(&cmd, copy_args, argc);
+
+// check cd	
+	printf("%s\n", getcwd(s, 100)); 
+	ft_cd(&cmd, all);
+	printf("%s\n", getcwd(s, 100));
+
+ 
+	return (0);
+}
