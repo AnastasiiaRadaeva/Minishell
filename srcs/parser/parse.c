ef7cc0738bb92c30199234a6be103660e1610eae
anatashi <anatashi@student.21-school.ru>
2020-11-30 22:13:28 +0300

    for vbox

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 15:19:04 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/30 22:12:24 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/11/30 22:13:26 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,9 +143,9 @@ static	void	add_nodes(t_commands **cmd, t_lexer *lexerbuf, t_data *data)
 		else
 		{
 			(*cmd)->redir = init_struct_commands(*cmd, data);
-			(*cmd) = (*cmd)->redir;
-			(*cmd)->previous = tmp_cmd;
 		}
+		(*cmd) = (*cmd)->redir;
+		(*cmd)->previous = tmp_cmd;
 		// if ((*cmd)->type_redir == CHAR_GREATER && (*cmd)->previous->type_redir == CHAR_GREATER)
 		// {
 		// 	if (!(*cmd)->cmd)
