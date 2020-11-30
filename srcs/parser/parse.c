8fe6ddebdf55479b15dd16ee404ea313ed074979
anatashi <anatashi@student.21-school.ru>
2020-11-30 20:22:47 +0300

    for vbox

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 15:19:04 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/30 20:18:51 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/11/30 20:22:44 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,11 +134,11 @@ static	void	add_nodes(t_commands **cmd, t_lexer *lexerbuf, t_data *data)
 	{	
 		init(cmd, data);
 		(*cmd)->type_redir = tmp->llisttok->type;
-		// if ((*cmd)->type_redir == CHAR_GREATER && (*cmd)->previous->type_redir == CHAR_GREATER)
-		// 	(*cmd)->type_redir = 3;
 		(*cmd)->redir = init_struct_commands(*cmd, data);
 		(*cmd) = (*cmd)->redir;
 		(*cmd)->previous = tmp_cmd;
+		if ((*cmd)->type_redir == CHAR_GREATER && (*cmd)->previous->type_redir == CHAR_GREATER)
+			(*cmd)->type_redir = 3;
 
 	}
 	else if (tmp->llisttok->type == CHAR_SEMICOLON)
