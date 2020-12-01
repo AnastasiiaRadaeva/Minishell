e268d7710640f66616f40ef1f431f1af31283406
anatashi <anatashi@student.21-school.ru>
2020-12-01 21:02:21 +0300

    rendering

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 15:19:04 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/27 18:32:51 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/01 21:02:17 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,12 +133,10 @@ static	void	add_nodes(t_commands **cmd, t_lexer *lexerbuf, t_data *data)
 	if (tmp->llisttok->type == CHAR_GREATER || tmp->llisttok->type == CHAR_LESSER)
 	{	
 		init(cmd, data);
+		(*cmd)->type_redir = tmp->llisttok->type;
 		(*cmd)->redir = init_struct_commands(*cmd, data);
 		(*cmd) = (*cmd)->redir;
 		(*cmd)->previous = tmp_cmd;
-		// (*cmd)->previous->type_redir = (*cmd)->previous->type_redir == CHAR_GREATER ? 3 : tmp->llisttok->type;
-		// if ((*cmd)->type_redir == CHAR_GREATER)
-			// (*cmd)->previous->type_redir = CHAR_GREATER;
 
 	}
 	else if (tmp->llisttok->type == CHAR_SEMICOLON)
