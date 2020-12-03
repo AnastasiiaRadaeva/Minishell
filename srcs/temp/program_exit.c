271eb20c05c9eae35e0f74c111a8a6ec1b951e7c
anatashi <anatashi@student.21-school.ru>
2020-12-03 11:44:19 +0300

    leaks check in ft_echo.c

--- srcs/temp/program_exit.c
+++ srcs/temp/program_exit.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 17:02:07 by anatashi          #+#    #+#             */
-/*   Updated: 2020/12/02 20:37:30 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/03 11:10:22 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	freeing_memory_from_struct_commands(t_commands *cmd)
 {
 	if (cmd)
 	{
-		ft_free_tmp(cmd->cmd);
+		// ft_free_tmp(cmd->cmd);
 		ft_free_tmp(cmd->cmd_dir);
 		ft_lstclear(&cmd->lst, ft_lstdelone_f);
 		if (cmd->previous)
@@ -24,6 +24,8 @@ void	freeing_memory_from_struct_commands(t_commands *cmd)
 		freeing_memory_from_struct_commands(cmd->redir);
 		freeing_memory_from_struct_commands(cmd->pipe);
 		freeing_memory_from_struct_commands(cmd->next);
+		free(cmd);
+		cmd = NULL;
 	}
 }
 
