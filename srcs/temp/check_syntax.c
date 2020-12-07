ed956ade4d05a65526df3cdd24202c1c5e74b41b
anatashi <anatashi@student.21-school.ru>
2020-12-07 11:10:56 +0300

    for git

--- srcs/temp/check_syntax.c
+++ srcs/temp/check_syntax.c
@@ -6,12 +6,12 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 15:39:42 by anatashi          #+#    #+#             */
-/*   Updated: 2020/12/05 20:39:02 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/07 11:09:52 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
-# include "../libft/header/libft.h"
+#include "minishell.h"
 
 static	int	check_doubl(t_tok *next)
 {
@@ -27,27 +27,28 @@ static	int	print_error(char *err, char *sym)
 {
 	ft_putstr(err);
 	ft_putstr(sym);
+	global_status = 2;
 	return (1);
 }
 
-static int	chek_one(t_tok *tok, int i)
+static int	chek_one(t_tok *tok)
 {
 	char 	*e;
 
-	if (*tok->data == ';' && (*tok->next->data == ';' || i == 1))
+	if (*tok->data == ';' && (*tok->next->data == ';' || *tok->next->data != ';'))
 		return (print_error(ERR_1, e = check_doubl(tok->next) ? "`;;'\n" : "`;'\n"));
-	if ((*tok->data == ';' || *tok->data == '>') && ((*tok->next->data == '>' ||
-		*tok->next->data == ';') || i == 1))
-		return (print_error(ERR_1, e = check_doubl(tok->next) ? "`;;'\n" : "`;'\n"));
-	if (*tok->data == '>' && !*tok->next->data  && !tok->next->next)
-		return (print_error(ERR_1, "`newline'\n"));
-	if (*tok->data == '>' && *tok->next->data == '>' && !*tok->next->next->data)
-		return (print_error(ERR_1, "`newline'\n"));
-	if (*tok->data != '>' && *tok->next->data == '>' && !*tok->next->next->data)
-		return (print_error(ERR_1, "`newline'\n"));
-	if (*tok->data != '>' && *tok->next->data == '>'
-		&& *tok->next->next->data == '>' && !*tok->next->next->next->data)
-		return (print_error(ERR_1, "`newline'\n"));
+	// if ((*tok->data == ';' || *tok->data == '>') && (*tok->next->data == '>' ||
+		// *tok->next->data == ';') || i == 1)
+	// 	return (print_error(ERR_1, e = check_doubl(tok->next) ? "`;;'\n" : "`;'\n"));
+	// if (*tok->data == '>' && !*tok->next->data  && !tok->next->next)
+	// 	return (print_error(ERR_1, "`newline'\n"));
+	// if (*tok->data == '>' && *tok->next->data == '>' && !*tok->next->next->data)
+	// 	return (print_error(ERR_1, "`newline'\n"));
+	// // if (*tok->data != '>' && *tok->next->data == '>' && !*tok->next->next->data)
+	// // 	return (print_error(ERR_1, "`newline'\n"));
+	// if (*tok->data != '>' && *tok->next->data == '>'
+	// 	&& *tok->next->next->data == '>' && !*tok->next->next->next->data)
+		// return (print_error(ERR_1, "`newline'\n"));
 	return (0);
 }
 
@@ -63,8 +64,8 @@ int			check_syntax(t_lexer *lexerbuf)
 	{
 		if (tok->next)
 		{
-			if (i == 1 || *tok->data == ';')
-				if (chek_one(tok, i))
+			if (i == 1)
+				if (chek_one(tok))
 					return (1);
 				
 			// if ((*tok->data == '>') && (*tok->next->data == '>'))
